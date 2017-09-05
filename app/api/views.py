c1e0baf8480defd63d9791d394fc9ab6abf74edc
Hyman95 <hy1995@gmail.com>
2017-09-05 23:31:59 +1000

    rewrite fetch answer data api

--- app/api/views.py
+++ app/api/views.py
@@ -6,6 +6,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from . import api
 from ..models import db
 from ..models import User, Survey, AnswerSurveyLink, Answer, Question
+import collections
 
 
 @api.route('/get_answer_data/type1/<int:survey_id>/<int:question_id>', methods=['GET', 'POST'])
@@ -13,30 +14,30 @@ def get_answer_data(survey_id, question_id):
     answer_survey_link = AnswerSurveyLink.query.filter_by(
         survey_id=survey_id).all()
     question = Question.get_by_id(question_id)
+
     vsa = {'label': 'Very Strongly Agree', 'value': 0}
     sa = {'label': 'Strongly Agree', 'value': 0}
     a = {'label': 'Agree', 'value': 0}
     d = {'label': 'Disagre', 'value': 0}
     sd = {'label': 'Strongly Disagree', 'value': 0}
     vsd = {'label': 'Very Strongly Disgree', 'value': 0}
+
+    rtn = [vsa, sa, a, d, sd, vsd]
+    rst = []
     for link in answer_survey_link:
         for answer in link.answers.all():
             if answer.question_id == question.id:
-                if answer.content == "Very Strongly Agree":
-                    vsa['value'] += 1
-                elif answer.content == "Strongly Agree":
-                    sa['value'] += 1
-                elif answer.content == "Agree":
-                    a['value'] += 1
-                elif answer.content == "Disagree":
-                    d['value'] += 1
-                elif answer.content == "Strongly Disagree":
-                    sd['value'] += 1
-                elif answer.content == "Very Strongly Disagree":
-                    vsd['value'] += 1
+                rst.append(answer.content)
 
+    counter = dict(collections.Counter(rst))
+    for key, value in counter.items():
+        for i in rtn:
+            if i['label'] == key:
+                i['value'] = value
+
+    print(rtn)
     rtn = {
-        "result": [vsa, sa, a, d, sd, vsd],
+        "result": rtn,
         "success": True
     }
     return jsonify(rtn)
