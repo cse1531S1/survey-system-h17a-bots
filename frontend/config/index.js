950df476d7333d709b643cc475dc47636e66c3ce
cvluca <z5155740@unsw.edu.au>
2017-10-23 20:32:19 +1100

    update

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -8,7 +8,7 @@ module.exports = {
 			index: path.resolve(__dirname, '../dist/index.html'),
 			assetsRoot: path.resolve(__dirname, '../dist'),
 			assetsSubDirectory: 'static',
-			assetsPublicPath: './',          //请根据自己路径配置更改
+			assetsPublicPath: './',
 			productionSourceMap: false,
 			// Gzip off by default as many popular static hosts such as
 			// Surge or Netlify already gzip all static assets for you.
